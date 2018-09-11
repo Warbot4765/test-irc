cd1b62d47c6c31b69cde0e2858b11c849c21b1d8
SwiperV <swiperv@gmail.com>
2018-09-10 19:16:42 -0500

    Edited a comment from within Eclipse

--- src/com/testing/simpleirc/Client.java
+++ src/com/testing/simpleirc/Client.java
@@ -22,7 +22,7 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
-// adding a comment here
+// editing comment in eclipse
 public class Client extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JTextArea history;
